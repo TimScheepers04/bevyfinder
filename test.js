0c2ed417d9c48a448699bccad0e12ce29c837a5e
Timothy Scheepers <timscheepers@Timothys-MacBook-Air.local>
2025-08-06 23:24:04 +0800

    Add simplest possible test for Render deployment debugging

---
+++ test.js
@@ -0,0 +1,5 @@
+console.log('Hello from Render!');
+console.log('Node.js version:', process.version);
+console.log('Current directory:', process.cwd());
+console.log('Environment:', process.env.NODE_ENV || 'development');
+process.exit(0); 
