7ae48989ffab62add7c5acb5d165a5e7c41b7ab9
TimScheepers04 <timothy.sheep823@gmail.com>
2025-09-24 12:42:29 +0800

    Remove static file serving to fix API routing

--- server-minimal.js
+++ server-minimal.js
@@ -294,9 +294,6 @@ app.post('/api/auth/login', async (req, res) => {
     }
 });
 
-// Serve static files (but not for API routes)
-app.use('/static', express.static(path.join(__dirname)));
-
 // Catch all handler - serve index.html for client-side routing (but not for API routes)
 app.get('*', (req, res) => {
     // Skip API routes
